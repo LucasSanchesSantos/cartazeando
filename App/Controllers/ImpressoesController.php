d981572079c4582e2ec49ecd6ac48414b0420e82
lucas sanches <lucassanches12398@hotmail.com>
2023-11-28 21:57:41 -0300

    apenas adicionando um foreach no insert de log de impressões

--- App/Controllers/ImpressoesController.php
+++ App/Controllers/ImpressoesController.php
@@ -15,40 +15,42 @@ class ImpressoesController extends Controller
         $data = json_decode(file_get_contents('php://input'), true);
     
         if (isset($data['dados']) && !empty($data['dados'])) {
-            $idFilial = $data['dados'][0]['id_filial'];
-            $idPromocao = $data['dados'][0]['id_promocao'];
-            $idProduto = $data['dados'][0]['id_produto'];
-            $idGradeX = $data['dados'][0]['id_grade_x'];
-            $idGradeY = $data['dados'][0]['id_grade_y'];
-            $idTipoPagamento = $data['dados'][0]['id_tipo_pagamento'];
-            $idPromocao = $data['dados'][0]['id_promocao'];
-            $precoAPrazo = $data['dados'][0]['preco_a_prazo'];
-            $promocao = $data['dados'][0]['promocao'];
-
-            IF($promocao == 'PREÇO PADRÃO'){   
-                $idPromocao = 0;
+            foreach ($data['dados'] as $indice => $item) {
+                $idFilial = $item['id_filial'];
+                $idPromocao = $item['id_promocao'];
+                $idProduto = $item['id_produto'];
+                $idGradeX = $item['id_grade_x'];
+                $idGradeY = $item['id_grade_y'];
+                $idTipoPagamento = $item['id_tipo_pagamento'];
+                $precoAPrazo = $item['preco_a_prazo'];
+                $promocao = $item['promocao'];
+        
+                if ($promocao == 'PREÇO PADRÃO') {
+                    $idPromocao = 0;
+                }
+        
+                $impressoes = new Impressoes([
+                    'id' => 0,
+                    'id_promocao' => $idPromocao,
+                    'id_produto' => $idProduto,
+                    'id_grade_x' => $idGradeX,
+                    'id_grade_y' => $idGradeY,
+                    'id_tipo_pagamento' => $idTipoPagamento,
+                    'preco_a_prazo' => $precoAPrazo,
+                    'id_filial' => $idFilial,
+                    'id_usuario' => 0,
+                    'data_hora_impressao' => date('Y-m-d H:i:s'),
+                ]);
+        
+                $impressoesDAO = new ImpressoesDAO();
+        
+                try {
+                    $impressoesDAO->cadastrar($impressoes);
+                } catch (\Exception $e) {
+                    // Tratamento de exceção, se necessário
+                }
             }
-
-            $impressoes = new Impressoes([
-                'id' => 0,
-                'id_promocao' => $idPromocao,
-                'id_produto' => $idProduto,
-                'id_grade_x' => $idGradeX,
-                'id_grade_y' => $idGradeY,
-                'id_tipo_pagamento' => $idTipoPagamento,
-                'preco_a_prazo' => $precoAPrazo,
-                'id_filial' => $idFilial,
-                'id_usuario' => 0,
-                'data_hora_impressao' => date('Y-m-d H:i:s'),
-            ]);
-    
-            $impressoesDAO = new ImpressoesDAO();
-    
-            try {
-                $impressoesDAO->cadastrar($impressoes);
-            } catch (\Exception $e) {
-            }
-        } 
+        }
     }
 
     public function index(): void
